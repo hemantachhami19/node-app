70844d1c9bfbeb1ecb390084dc159da8786ff831
Hemant Achhami <hemantachhami19@gmail.com>
2018-10-26 07:48:25 +0545

    Simple routes set up

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const  express  = require('express');
-var app  = express();
+const express = require('express');
+var app = express();
 
-app.get('/', function (req,res) {
-res.send("Basic server setup")
+app.get('/', function (req, res) {
+    res.send("<h1>Index page</h1>")
 });
+app.get('/about', function (req, res) {
+    res.send("<h1>About page</h1>")
+});
+app.get('/send-json', function (req, res) {
+    res.send({
+       name:"hemant",
+       age:24
+    });
+});
+
+app.get('/bad', function (req, res) {
+    res.send({
+       errorMessage: "Unable to handle the request"
+    });
+});
+
 //listen helps to bind the server in port of machine
 app.listen(3000);
